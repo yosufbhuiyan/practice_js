5a16c9fd544677f053dc3049904dc23f3c785460
Yosuf Bhuiyan <yosufbhuiyanbusiness@gmail.com>
2023-01-10 23:54:52 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+var a ;
+
+a = 14;
+
+console.log(a)
+
+let q;
+q = 12;
+
+console.log(q)
+
+
+const u = 17;
+console.log(u)
+
+function ami(name,age){
+    console.log('name' + "this is me"+ 'age')
+    return(name, age);
+}
+
+console.log('jasim',17)
